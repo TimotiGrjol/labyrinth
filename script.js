d43d7ae0a961b928c03d4edb9c46edfc1ae92842
Timoti Grjol <79981922+TimotiGrjol@users.noreply.github.com>
2023-04-03 23:42:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -198,14 +198,14 @@ function getKey(){
         title: "You got the key!",
         icon: "success",
         confirmButtonColor: "darkgreen"
-      }).then(function(isConfirm) {
+      },500).then(function(isConfirm) {
         let x=420;
         let y=360;
         ctx2.clearRect(x,y,20,20);
         map2[18][21]='.';
         masKey=1;
           
-        },500);
+        });
 
     
 }
